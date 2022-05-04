a56f84313c59fe382df5305d4a670197f8f62f52
Trygve Aspenes <trygveas@met.no>
2022-05-04 12:01:26 +0200

    fix and add tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,13 @@
 import importlib
 import os
 from unittest.mock import patch
+from xmlrpc.client import DateTime
 import pytest
 import shutil
 from fastapi.testclient import TestClient
 #import unittest.mock as mock
 import base64
-
+import datetime
 from mapgen.main import app
 
 client = TestClient(app)
@@ -29,7 +30,8 @@ def run_before_and_after_tests():
     # Need some envs
     os.environ['S3_ACCESS_KEY'] = "test s3 access key"
     os.environ['S3_SECRET_KEY'] = "test s3 secret key"
-    
+    os.environ['S3_ENDPOINT_URL'] = "test S3_ENDPOINT_URL"
+    os.environ['S3_TENANT'] = "test S3_TENANT"
     yield # this is where the testing happens
 
     # Teardown : fill with any logic you want
@@ -54,12 +56,18 @@ def test_read_main_with_config_dict():
     assert response.text == ""
 
 path = "/api/get_mapserv/satellite-thredds/polar-swath/2022/04/27/"
+@patch('boto3.client')
 @patch('mapgen.api.redirect.upload_mapfile_to_ceph')
 @pytest.mark.parametrize("netcdf_path", ["metopb-avhrr-20220427124247-20220427125242.nc", "metopc-avhrr-20220427115541-20220427120710.nc", "noaa19-avhrr-20220427121037-20220427121853.nc",
                                          "noaa20-viirs-mband-20220427113327-20220427114740.nc", "noaa20-viirs-iband-20220427113327-20220427114740.nc", "noaa20-viirs-dnb-20220427113327-20220427114740.nc",
                                          "npp-viirs-mband-20220427122315-20220427123728.nc", "npp-viirs-iband-20220427122315-20220427123728.nc", "npp-viirs-dnb-20220427122315-20220427123728.nc"])
-def test_get_netcdf(upload_patch, netcdf_path):
+def test_get_netcdf(upload_patch, boto3_client, netcdf_path):
     upload_patch.return_value = True
+    boto3_client().list_objects.return_value = {'Contents': [{'Key': '2022/04/27/overview_20220427_124247.tif'},
+                                                             {'Key': '2022/04/27/overview_20220427_115541.tif'},
+                                                             {'Key': '2022/04/27/overview_20220427_113327.tif'},
+                                                             {'Key': '2022/04/27/overview_20220427_122315.tif'},
+                                                             {'Key': '2022/04/27/overview_20220427_121037.tif'}]}
     response = client.get(path + netcdf_path, allow_redirects=False)
     print(response.text)
     assert response.status_code == 307
@@ -110,3 +118,47 @@ def test_get_dashboard():
     print(response.text)
     print(response.status_code)
     assert response.status_code == 200
+
+@patch('boto3.client')
+def test_list_files_in_bucket(boto3_client):
+    from mapgen.modules.satellite_thredds_module import list_files_in_bucket
+    boto3_client().list_objects.return_value = {'Contents': [{'Key': '2022/04/27/overview_20220427_124247.tif'},
+                                                             {'Key': '2022/04/27/overview_20220427_115541.tif'},
+                                                             {'Key': '2022/04/27/overview_20220427_113327.tif'},
+                                                             {'Key': '2022/04/27/overview_20220427_122315.tif'},
+                                                             {'Key': '2022/04/27/overview_20220427_121037.tif'}]}
+    bucket = 'test bucket'
+    start_time = datetime.datetime(2022, 4, 27, 12, 42, 47)
+    files_in_bucket = list_files_in_bucket(bucket, start_time)
+    expected_files = ['2022/04/27/overview_20220427_124247.tif', '2022/04/27/overview_20220427_115541.tif', '2022/04/27/overview_20220427_113327.tif', '2022/04/27/overview_20220427_122315.tif', '2022/04/27/overview_20220427_121037.tif']
+    assert files_in_bucket == expected_files
+
+@patch('boto3.client')
+def test_list_files_in_bucket_exception(boto3_client):
+    from mapgen.modules.satellite_thredds_module import list_files_in_bucket
+    boto3_client().list_objects.side_effect = Exception("boto3 client list_bucket exception")
+    bucket = 'test bucket'
+    start_time = datetime.datetime(2022, 4, 27, 12, 42, 47)
+    files_in_bucket = list_files_in_bucket(bucket, start_time)
+    expected_files = []
+    assert files_in_bucket == expected_files
+
+@patch('mapgen.modules.satellite_thredds_module.list_files_in_bucket')
+def test_get_previews(list_files_in_bucket):
+    from mapgen.modules.satellite_thredds_module import get_previews
+    files_in_bucket = ['2022/04/27/overview_20220427_124247.tif', '2022/04/27/overview_20220427_115541.tif', '2022/04/27/overview_20220427_113327.tif', '2022/04/27/overview_20220427_122315.tif', '2022/04/27/overview_20220427_121037.tif']
+    list_files_in_bucket.return_value = files_in_bucket
+    start_time = datetime.datetime(2022, 4, 27, 12, 42, 47)
+    regexp_pattern_module = {'geotiff_bucket': 'test bucket'}
+    previews = get_previews(start_time, regexp_pattern_module)
+    assert previews == ['2022/04/27/overview_20220427_124247.tif']
+
+def test_build_render_data():
+    from mapgen.modules.satellite_thredds_module import build_render_data
+    start_time = datetime.datetime(2022, 4, 27, 12, 42, 47)
+    regexp_pattern_module = {'geotiff_bucket': 'test bucket'}
+    previews = ['2022/04/27/overview_20220427_124247.tif']
+    layers_render_data = build_render_data(regexp_pattern_module, start_time, previews)
+    assert layers_render_data == [{'preview': '/vsicurl/test S3_ENDPOINT_URL/test S3_TENANT:test bucket/2022/04/27/overview_20220427_124247.tif',
+                                   'preview_stamp': '2022-04-27T12:42:47Z',
+                                   'layer_name': 'overview'}]
